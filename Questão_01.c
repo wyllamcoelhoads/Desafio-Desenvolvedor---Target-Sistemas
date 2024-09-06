d4a2224a3327ced4c3070c6745dc88f7b1a22794
wyllamcoelhoads <wyllamcoelho8@gmail.com>
2024-09-06 18:39:46 -0300

    Finalizando a questão 01

---
+++ Questão_01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int INDICE = 12, SOMA = 0, K = 1;
+
+    while (K < INDICE) {
+        K = K + 1;
+        SOMA = SOMA + K;
+    }
+
+    printf("%d\n", SOMA);
+
+    return 0;
+}
